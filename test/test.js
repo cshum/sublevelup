c61984b8f72cb161c719bf8ba5cc26c7c771847e
Adrian Shum <me@cshum.com>
2015-08-21 14:06:55 +0800

    preparing new test

--- test/test.js
+++ test/test.js
@@ -23,3 +23,57 @@ test('location', function(t){
   t.equal(dbA.options.keyEncoding, 'binary', 'extend option');
   t.end();
 });
+
+return;
+
+test('Default', function(t){
+  var sub = sublevelup(db);
+
+  var foo = sub('foo');
+  var hello = sub('hello');
+  var fooBar = sub(foo, 'bar');
+  var fooBarBla = sub(fooBar, 'bla');
+
+  t.equal(foo.location, '!foo!', 'base sub');
+  t.equal(hello.location, '!hello!', 'base sub');
+  t.equal(fooBar.location, '!foo#bar!', 'nested sub');
+  t.equal(fooBarBla.location, '!foo#bar#bla!', 'double nested sub');
+});
+
+test('Custom Prefix', function(t){
+  var sub = sublevelup(db, function(prefix){
+    return prefix.map(function(name){
+      return '!'+name+'!';
+    }).join();
+  });
+
+  var foo = sub('foo');
+  var hello = sub('hello');
+  var fooBar = sub(foo, 'bar');
+  var fooBarBla = sub(fooBar, 'bla');
+
+  t.equal(foo.location, '!foo!', 'base sub');
+  t.equal(hello.location, '!hello!', 'base sub');
+  t.equal(fooBar.location, '!foo!!bar!', 'nested sub');
+  t.equal(fooBarBla.location, '!foo!!bar!!bla!', 'double nested sub');
+});
+
+
+test('LevelDOWN', function(t){
+  var sub = sublevelup(my('db', {
+    host: 'localhost',
+    user: 'root'
+  }), function(prefix){
+    return prefix.join('_');
+  });
+
+  var foo = sub('foo');
+  var hello = sub('hello');
+  var fooBar = sub(foo, 'bar');
+  var fooBarBla = sub(fooBar, 'bla');
+
+  t.equal(foo.location, 'foo', 'base sub');
+  t.equal(hello.location, 'hello', 'base sub');
+  t.equal(fooBar.location, 'foo_bar', 'nested sub');
+  t.equal(fooBarBla.location, 'foo_bar_bla', 'double nested sub');
+});
