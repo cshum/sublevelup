ad9b0791ee56e3b22b190ce2b4452e28deb0c241
Adrian Shum <me@cshum.com>
2015-08-23 11:07:34 +0800

    tweak levelup detection

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function Sublevel (db, name, options) {
       //Passing sublevel return sublevel
       return db;
     }
-  }else if(db.toString() === 'LevelUP'){
+  }else if(db instanceof LevelUP){
     //root is LevelUP, prefix based
     defaults.prefixEncoding = prefixCodec;
     override.db = prefix(db);
